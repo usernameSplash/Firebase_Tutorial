58b873c08498cede64b0919c6dfe41ac421eeeff
Kyeongmin Kim <kkfenix3085@naver.com>
2021-01-28 00:09:43 +0900

    Remove Useless Files

--- firebase_tutorial/src/App.js
+++ firebase_tutorial/src/App.js
@@ -3,21 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Kyeongmin Kim
-        </p>
-        <a
-          href="https://www.google.com/search?q=lionel+messi"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lionel Messi
-        </a>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
