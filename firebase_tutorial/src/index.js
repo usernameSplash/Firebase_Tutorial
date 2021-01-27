2e3a1bc33375633819f31220776f6ba80ad736cc
Kyeongmin Kim <kkfenix3085@naver.com>
2021-01-28 00:23:02 +0900

    Initialize Firebase App

--- firebase_tutorial/src/index.js
+++ firebase_tutorial/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import firebase from "./Firebase"
+
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
